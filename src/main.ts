22ed89089905ac854486d4199ac715864a2bbeae
Raprast <raprast.raka@gmail.com>
2025-05-20 13:59:09 +0800

    ketinggalan

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionsFilter());
   app.enableCors({
-    origin: ['http://localhost:5173'], // Change to your Next.js frontend origin
+    origin: ['http://localhost:5173', 'https://kilan-admin.netlify.app'], // Change to your Next.js frontend origin
     credentials: true, // If you're sending cookies or auth headers
   });
   await app.listen(process.env.PORT ?? 1243);
